f6e10e44320fd73d37ff548350e1019578bd1f3b
Yury <yurybalrogwar@gmail.com>
2019-11-02 15:01:34 +0300

    add adminDao

--- dao/src/main/java/dao/impl/AdminDAOImpl.java
+++ dao/src/main/java/dao/impl/AdminDAOImpl.java
@@ -1,6 +1,55 @@
 package dao.impl;
 
 import dao.interfaces.AdminDAO;
+import entity.User;
 
 public class AdminDAOImpl implements AdminDAO {
+
+    public String getAllUsers() {
+        return null;
+    }
+
+    public void deleteUserByLogin(String login) {
+
+    }
+
+    public void editProfile(User user) {
+
+    }
+
+    public void addNewUser() {
+
+    }
+
+    public String getAllStations() {
+        return null;
+    }
+
+    public void deleteStationByName(String name) {
+
+    }
+
+    public void editStationIntineary(String stationName) {
+
+    }
+
+    public void addNewStation() {
+
+    }
+
+    public String getAllTransports() {
+        return null;
+    }
+
+    public void deleteTransportById(long id) {
+
+    }
+
+    public void editTransport(long id) {
+
+    }
+
+    public void addNewTransport() {
+
+    }
 }
--- dao/src/main/java/dao/interfaces/AdminDAO.java
+++ dao/src/main/java/dao/interfaces/AdminDAO.java
@@ -1,5 +1,24 @@
 package dao.interfaces;
 
+import entity.Station;
+import entity.User;
+
 public interface AdminDAO {
+    //Users
+    String getAllUsers();
+    void deleteUserByLogin(String login);
+    void editProfile(User user);
+    void addNewUser();
+    //Stations
+    String getAllStations();
+    void deleteStationByName(String name);
+    void editStationIntineary(String stationName);
+    void addNewStation();
+    //Transport
+    String getAllTransports();
+    void deleteTransportById(long id);
+    void editTransport(long id);
+    void addNewTransport();
+
 
 }
